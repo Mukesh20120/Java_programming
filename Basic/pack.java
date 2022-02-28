54ef9af47a8850bb8b89e5b0b0c5735afb1a8c3d
Free_spirit <mukeshsharma20120@gmail.com>
2022-02-28 23:54:42 +0530

    little bit about package

---
+++ Basic/pack.java
@@ -0,0 +1,14 @@
+//import java.lang.String;
+import java.util.*;
+public class pack {
+     public static void main(String args[]){
+         Scanner sc=new Scanner(System.in);
+
+        // String str;
+        //if we not include the library file we can directly use the function
+        java.lang.String  str;
+         str=sc.nextLine();
+          System.out.println(str);
+          sc.close();
+     }
+}
